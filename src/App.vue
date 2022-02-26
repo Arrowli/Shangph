e116819711d3ef1b1163d3a1edbcea660ad9b5c9
Arrowli <413740083@qq.com>
2022-02-26 11:07:45 +0800

    shangph create

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Header/>
+    <router-view/>
+    <Footer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Footer from './components/Footer/Footer.vue'
+import Header from './components/Header/Header.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    Footer,
+    Header,
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/pages/Home/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <ListContainer/>
+      <Floor/>
+      <Recommend/>
+      <Like/>
+      <Rank/>
+      <Brand/>
+  </div>
+</template>
+
+<script>
+import Brand from '@/pages/Home/Brand'
+import Floor from '@/pages/Home/Floor'
+import Like from '@/pages/Home/Like'
+import ListContainer from '@/pages/Home/ListContainer'
+import Rank from '@/pages/Home/Rank'
+import Recommend from '@/pages/Home/Recommend'
+
+export default {
+  components: {
+    Brand,
+    Floor,
+    Like,
+    ListContainer,
+    Rank,
+    Recommend
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
